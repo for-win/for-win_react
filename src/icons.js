d934b6a229b9d114d6f607edd3247a106e8eca6d
jinhyeok15 <ekfrhd97@gmail.com>
2021-08-16 07:42:33 +0900

    lol favicon 추가

--- src/icons.js
+++ src/icons.js
@@ -4,3 +4,5 @@ import AiOutlineLock from 'react-icons/ai';
 import AiOutlineUnlock from 'react-icons/ai';
 // 세팅
 import AiTwotoneSetting from 'react-icons/ai';
+// 롤 favicon
+// <link data-react-helmet="true" rel="icon" href="https://lolstatic-a.akamaihd.net/frontpage/apps/prod/harbinger-l10-website/ko-kr/production/ko-kr/static/favicon-0cf29ce019f7cd1e7b24f85ab6ff97da.ico"></link>

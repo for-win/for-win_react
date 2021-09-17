d13fd31c4741e567e782d33fbde148ebcff94e72
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-17 20:47:35 +0900

    API failed to fetch 이슈

--- src/app/lobby/api/room.js
+++ src/app/lobby/api/room.js
@@ -1,37 +1,43 @@
 import React, { useState, useEffect, useCallback } from "react";
 
 function RoomList(props) {
-    const [rooms, setRooms] = useState(null);
+    const [rooms, setRooms] = useState([]);
     const [error, setError] = useState(null);
 
     const fetchRooms = useCallback(
-        async () => {
-            const res = await fetch('http://3.37.245.109:3000/room/leagueoflegends');
-            try {
-                setRooms(res.data);
-            } catch (e) {
-                console.log(e);
-                setError(e);
-            }
+        () => {
+            fetch('http://3.37.245.109:3000/room/leagueoflegends')
+            .then(res => {
+                return res.json();
+            })
+            .then(
+                (result) => {
+                setRooms(result.data);
+                },
+                (error) => {
+                    setError(error);
+                }
+            )
         },
         [],
     );
 
     useEffect(()=> {
         fetchRooms();
-    }, [fetchRooms]);
-    if (error) return <div>{error.message}</div>;
-    if (!rooms) return <div>{rooms}</div>;
+    }, []);
+    if (error) return <div>{error.toString()}</div>;
+    if (!rooms) return <div>nodata</div>;
 
     return (
         <tables>
             <div>success</div>
             {rooms.map(room => (
                 <tr>
-                    <td>{room.secret}</td>
+                    {/* <td>{room.secret}</td>
                     <td>{room.code}</td>
                     <td>{room.memberCnt}/{room.max}</td>
                     <td>{room.type}</td>
+                    <td>{room.user.name}</td> */}
                     <td>{room.user.name}</td>
                 </tr>
             ))}

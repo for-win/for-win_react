eb6a420a505845858b19629c09dfb4e4e415823a
jinhyeok15 <ekfrhd97@gmail.com>
2021-09-18 18:13:32 +0900

    text()로 원인 확인

--- src/app/lobby/api/room.js
+++ src/app/lobby/api/room.js
@@ -1,18 +1,21 @@
 import React, { useState, useEffect, useCallback } from "react";
+import fetch from 'fetch-with-proxy';
 
 function RoomList(props) {
     const [rooms, setRooms] = useState([]);
     const [error, setError] = useState(null);
+    const [status, setStatus] = useState('');
 
     const fetchRooms = useCallback(
         () => {
             fetch('http://3.37.245.109:3000/room/leagueoflegends')
             .then(res => {
-                return res.json();
-            })
+                setStatus(res.status);
+                return res.text()})
             .then(
                 (result) => {
-                setRooms(result.data);
+                    setRooms(result);
+                    console.log(result);
                 },
                 (error) => {
                     setError(error);
@@ -25,22 +28,21 @@ function RoomList(props) {
     useEffect(()=> {
         fetchRooms();
     }, []);
-    if (error) return <div>{error.toString()}</div>;
+    if (error) return <div>{rooms}</div>;
     if (!rooms) return <div>nodata</div>;
 
     return (
         <tables>
-            <div>success</div>
-            {rooms.map(room => (
+            <div>{rooms}</div>
+            {/* {rooms.data.map(room => (
                 <tr>
-                    {/* <td>{room.secret}</td>
+                    <td>{room.secret}</td>
                     <td>{room.code}</td>
                     <td>{room.memberCnt}/{room.max}</td>
                     <td>{room.type}</td>
-                    <td>{room.user.name}</td> */}
                     <td>{room.user.name}</td>
                 </tr>
-            ))}
+            ))} */}
         </tables>
     );
 }
